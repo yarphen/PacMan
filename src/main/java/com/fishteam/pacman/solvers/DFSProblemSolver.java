fcacdd6f2d22f6019d073cfde390af3dd33fcab7
yarphen <leviatan.ka14@gmail.com>
2016-10-06 22:27:33 +0300

    Fixing DFS solver

---
+++ src/main/java/com/fishteam/pacman/solvers/DFSProblemSolver.java
@@ -0,0 +1,39 @@
+package com.fishteam.pacman.solvers;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+
+import com.fishteam.pacman.interfaces.*;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class DFSProblemSolver implements ProblemSolver{
+	public List<ProblemState> solve(final Problem problem){
+		Set<ProblemState> visited = new HashSet<ProblemState>();
+		return findFromHead(problem.getStartState(), new Function<ProblemState, List<ProblemState>>() {
+			public List<ProblemState> apply(ProblemState t) {
+				return problem.getChildren(t);
+			}
+		}, visited, problem.getGoalState());
+	}
+	private List<ProblemState> findFromHead(ProblemState current, Function<ProblemState, List<ProblemState>> childFunction, Set<ProblemState> visited, ProblemState goal){
+		if (current.equals(goal)){
+			LinkedList<ProblemState> result = new LinkedList<>();
+			result.add(current);
+			return result;
+		}
+		List<ProblemState> children = childFunction.apply(current);
+		for(ProblemState state: children){
+			if (!visited.contains(state)){
+				visited.add(state);
+				List<ProblemState> way = findFromHead(state, childFunction, visited, goal);
+				if (way!=null){
+					way.add(current);
+					return way;
+				}
+			}
+		}
+		return null;
+	}
+}
